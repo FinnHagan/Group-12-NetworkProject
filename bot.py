1f231c2b6a2f87c3f2c315b5b8ecc220f1fbcacb
FinnHagan <2410686@dundee.ac.uk>
2022-10-19 18:17:47 +0100

    Fixed previous errors and added in !slap command
    Code neatened up and all working bar efficient handling of users in channel and some more common network handling

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import socket, time, argparse, random, datetime
 #Connect to miniircd python miniircd --ipv6 --debug
+
 #Initialising variables
 PORT = 6667
 HOST = "::1" #fc00:1337::17/96 is the host we will use, ::1 is for testing purposes
@@ -50,8 +51,7 @@ def connect():
         #Sending client information to the server
         s.send(bytes("USER "+ NICK + " "+ NICK +" "+ NICK + " :python\r\n", "UTF-8"))
         s.send(bytes("NICK "+ NICK + "\r\n", "UTF-8")) #Sets the bots nick name
-        connect = s.recv(2048).decode("UTF-8") #Recieves the bots nickname
-        print("BOT information has been successfully sent to the server." + connect)      
+        print("BOT information has been successfully sent to the server.")      
     except:
         print("BOT information was unable to be sent to the server, please try again.")
         exit()
@@ -63,9 +63,7 @@ def join(chan):
         print("Attempting to join channel... ")
         #Join the channel
         s.send(bytes("JOIN "+ chan + "\r\n", "UTF-8"))
-        join = s.recv(2048).decode("UTF-8")
-        print("Successfully joined: " + join)
-        display_users()
+        print("Successfully joined channel.")
     except:
         print("Unable to join channel: ")
         exit()
@@ -80,13 +78,14 @@ def message_split(message):
     return message
 
 #Function to display the users in the channel
-def display_users():
-    s.send(bytes("NAMES " + CHANNEL + "\r\n", "UTF-8"))
-    users = s.recv(2048).decode("UTF-8")
-    nickName = users.split(' :',1)[1].split('\n', 1)[0]
-    userList.append(nickName)
-    print("Users in: " + CHANNEL + " are: " + nickName)
-    print("\n")
+def display_users(msg):
+    for i in range(5, len(msg)):
+        if i == 5:
+            userList.append(msg[i][1:])
+            continue
+        userList.append(msg[i])
+    print("Current users in the channel: ")
+    print(userList)
 
 #Get random fact from text file
 def get_random_fact():
@@ -98,36 +97,52 @@ def get_random_fact():
 def send_message(message, target):
     s.send(bytes("PRIVMSG " + target + " :" + message + "\r\n", "UTF-8"))
 
+#Get a random user from the channel
+def get_random_user():
+    user = random.choice(userList)
+    return user
+
 #Process messages as per the their string format
-def process_message():
-    msg = s.recv(2048).decode("UTF-8")
-    name = msg.split('!',1)[0][1:]
-    message = msg.split('PRIVMSG',1)[1].split(':',1)[1] #Get the message
-    destination = msg.split(' ',3)[2] #What channel or user the message is directed to
-    
-    if len(name) < 17: #Checks if there is a valid user
-        
+def process_message(msg):
+    origin= msg[0][1:].split("!",1)[0]
+    message = msg[3] #Get the message
+    destination = msg[2] #What channel or user the message is directed to
+
+    if len(origin) < 17:
+
         #Lets us know that the message is either a private message or a channel message
         if destination.lower() == CHANNEL.lower():
+            #If 
             if message.find("!hello") != -1:
                 greeting = datetime.datetime.now().strftime('%d-%m-%y %H:%M')
-                send_message("Hello " + name + " it is currently " + greeting + " ", destination)
+                send_message("Hello " + origin + " it is currently " + greeting + " ", destination)
+
+            if message.find("!slap") != -1:
+                randomSlap = get_random_user()
+                send_message("slaps " + randomSlap + " around a bit with a large trout", destination)
 
         #Lets us know it is a private message        
         if destination.lower() == NICK.lower():
+            #Hence respond with a random fact
             if message != -1:
-                #Hence respond with a random fact
                 fact = get_random_fact()
-                send_message(fact, name)
+                send_message(fact, origin)
 
 #Function to handle separate commands
 def handle_commands(msg):
-    if msg[0] == "PING":
-        ping_pong(msg[0])   
-    elif msg[1] == "PRIVMSG":
-        process_message()
-    elif msg[0] == "NAMES":
-        display_users()
+    # print(msg)
+    try:
+        #If the message is a PING, then PONG back
+        if msg[0] == "PING":
+            ping_pong(msg[0])
+        #If a PRIVMSG, then process the message   
+        elif msg[1] == "PRIVMSG":
+            process_message(msg)
+        #If a 353, then display the users in the channel
+        elif msg[1] == "353":
+            display_users(msg)
+    except:
+        return
         
 #Main function
 def main():  
@@ -136,10 +151,12 @@ def main():
   #Infinite while loop to keep the bot running
   while True:
     #Parses the seperate commands
-    msg = s.recv(2048).decode("UTF-8")   
-    print(msg)
-    msg = message_split(msg)
-    handle_commands(msg)
+    info = s.recv(2048).decode("UTF-8")
+    messages = info.split("\r\n")
+    for msg in messages:   
+        print(msg)
+        msg = message_split(msg)
+        handle_commands(msg)
     
     #Add in handling of arguments into this loop too.
 
